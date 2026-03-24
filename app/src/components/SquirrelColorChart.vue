62a040be9bc314d89a54db5e5a9fa2c3af357137
alexavso <alexasolanou@gmail.com>
2026-03-24 12:31:29 -0400

    .

---
+++ app/src/components/SquirrelColorChart.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Squirrel Sightings By Color</h2>
+  </div>
+</template>
+
+<script setup>
+
+
+</script> 
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SquirrelData from '@/views/SquirrelData.vue'
+import SquirrelColorChart from '@/components/SquirrelColorChart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'squirreldata',
       component: SquirrelData,
     },
+    {
+      path: '/',
+      name: 'squirrelcolors',
+      component: SquirrelColorChart
+    }
     
   ],
 })
