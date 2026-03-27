e78438352e384f770c7009b4a2aa8f51fb9c435f
alexavso <alexasolanou@gmail.com>
2026-03-27 12:31:53 -0400

    .

--- app/src/components/SquirrelColorChart.vue
+++ app/src/components/SquirrelColorChart.vue
@@ -2,10 +2,16 @@
   <div>
     <h2>Squirrel Sightings By Color</h2>
   </div>
+  <div class="chart">
+    <Doughnut :data="chartData" :options="ChartOptions"  />
+  </div>
 </template>
 
 <script setup>
-import Chart from 'chart.js/auto';
+import { ref, onMounted } from 'vue'
+import { Doughnut } from 'vue-chartjs'
+
+
 
 
 
